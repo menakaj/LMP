8057a2178b4fdf4e3153f26780527727b9d4a812
tharinda <tharinda221@gmail.com>
2016-05-29 00:44:25 +0530

    Mac device enroll API added

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+import socket
+
 from config import *
 
 if __name__ == '__main__':
     context = (path_to_certificates + 'Server.crt', path_to_certificates + 'Server.key')
-    app.run(host="172.22.111.214", port=8080, ssl_context=context)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 0))
+    ipAddress = s.getsockname()[0]
+    app.run(host=ipAddress, port=8080, ssl_context=context)
