03d48bd64c3e36473ac8d28a8956b63fef1ebc3e
irunika <irunikail@gmail.com>
2016-05-29 00:13:42 +0530

    Added Home Page Enrolled Devices

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Render_Index, '/')
+api.add_resource(EnrollDevice, '/enroll-device/<device_type>')
--- uicontroller/UIController.py
+++ uicontroller/UIController.py
@@ -3,5 +3,12 @@ from __init__ import *
 class Render_Index(Resource):
     def get(self):
         return make_response(
-            render_template("index.html")
+            render_template("home.html")
+        )
+
+class EnrollDevice(Resource):
+    def get(self, device_type):
+        print 'Request to enroll a ' + device_type + ' device'
+        return  make_response(
+            render_template("enrollDevice.html")
         )
