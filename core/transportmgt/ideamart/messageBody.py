da729a6d32da3cfbff0df6c437a279d1743009f4
menaka <menaka12350@gmail.com>
2016-05-29 03:50:58 +0530

    Added license header

--- core/transportmgt/ideamart/messageBody.py
+++ core/transportmgt/ideamart/messageBody.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Team - iAS, University Of Peradeniya
+# Copyright 2016 Team - LMP, University Of Peradeniya
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
